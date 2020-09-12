765b7916258c2de05712a9a5b3a0efbbf105f8b7
Kemenike <kanayoemenike37@gmail.com>
2020-09-12 00:29:31 -0700

    adding php file

---
+++ lab3.php
@@ -0,0 +1,13 @@
+<?php
+$param = $GET['param'];
+echo "param is: " . $param . "<br>";
+if($param == "hello") {
+    echo "hey there! <br>";
+}
+else { 
+    $num = $param*2+3;
+    echo "number:" . $num . "<br>";
+}
+echo "<br>";
+echo "<img src=\"diagram1.gif\"/";
+?>
